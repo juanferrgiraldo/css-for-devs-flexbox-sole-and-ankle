4a377210e51dab5da13b8e0af66e029f94fbefa3
Juanfis <juanferrgiraldo@gmail.com>
2023-05-22 11:35:19 -0500

    feat: mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,11 +12,6 @@ import VisuallyHidden from '../VisuallyHidden/VisuallyHidden';
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-
   return (
     <header>
       <SuperHeader />
@@ -41,7 +36,7 @@ const Header = () => {
             <Icon id='search' strokeWidth={1} />
             <VisuallyHidden>Search</VisuallyHidden>
           </UnstyledButton>
-          <UnstyledButton>
+          <UnstyledButton onClick={() => setShowMobileMenu(true)}>
             <Icon id='menu' strokeWidth={1} />
             <VisuallyHidden>Menu</VisuallyHidden>
           </UnstyledButton>
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -6,6 +6,7 @@ import { DialogOverlay, DialogContent } from '@reach/dialog';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+import { COLORS, WEIGHTS } from '../../constants';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   if (!isOpen) {
@@ -13,23 +14,91 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay>
+      <Content>
+        <CloseButton onClick={onDismiss}>
+          <Icon id='close' />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </CloseButton>
+        <Filler />
+        <Nav>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
+        </Nav>
+        <Footer>
+          <FooterLink href='/terms'>Terms and Conditions</FooterLink>
+          <FooterLink href='/privacy'>Privacy Policy</FooterLink>
+          <FooterLink href='/contact'>Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: hsl(220deg 5% 40% / 0.8);
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Content = styled(DialogContent)`
+  background: white;
+  width: 300px;
+  height: 100%;
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 10px;
+  right: 0px;
+  padding: 16px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const NavLink = styled.a`
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+  font-size: 1.125rem;
+  text-decoration: none;
+  text-transform: uppercase;
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1;
+`;
+
+const Footer = styled.footer`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+const FooterLink = styled.a`
+  color: ${COLORS.gray[700]};
+  text-decoration: none;
+  padding: 7px;
+`;
 
 export default MobileMenu;
